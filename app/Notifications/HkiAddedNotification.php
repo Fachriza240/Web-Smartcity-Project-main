fdefbfe5476d3fe4fff533534b805073469ff538
Jannah Kurniawati <jannahkurnia15@gmail.com>
2026-09-25 09:04:59 +0700

    Refactor HkiAddedNotification and AppServiceProvider by removing unnecessary comments and enhancing code clarity

--- app/Notifications/HkiAddedNotification.php
+++ app/Notifications/HkiAddedNotification.php
@@ -14,27 +14,16 @@ class HkiAddedNotification extends Notification
 
     protected $hki;
 
-    /**
-     * Create a new notification instance.
-     */
     public function __construct(Hki $hki)
     {
         $this->hki = $hki;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @return array<int, string>
-     */
     public function via(object $notifiable): array
     {
         return ['database'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     */
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
@@ -43,11 +32,6 @@ class HkiAddedNotification extends Notification
             ->line('Thank you for using our application!');
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @return array<string, mixed>
-     */
     public function toArray(object $notifiable): array
     {
         return [
