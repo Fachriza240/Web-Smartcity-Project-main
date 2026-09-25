831e6de5e378f80044812a5363bd6b67131bbe7e
Jannah Kurniawati <jannahkurnia15@gmail.com>
2026-09-25 09:08:33 +0700

    Remove unnecessary comments from test files to enhance code clarity

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,12 +7,8 @@ use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-
     use RefreshDatabase;
 
-    /**
-     * A basic test example.
-     */
     public function test_the_application_returns_a_successful_response(): void
     {
         $response = $this->get('/');
